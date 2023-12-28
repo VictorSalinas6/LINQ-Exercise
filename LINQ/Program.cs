87852a6703344919015eddd6e686731d8934db49
Victor Salinas <victorsalinas6@gmail.com>
2023-12-28 12:24:38 -0600

    Created the list, ordered and displayed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videogames = new string[] { "God of War", "Kingdom Hearts 3", "Genshin Impact", "It Takes Two", "Xenoblade", "Super Smash Brothers" };
+
+            videogames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
